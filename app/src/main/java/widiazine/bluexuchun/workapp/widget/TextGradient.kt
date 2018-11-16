be56481ecdbe33e40ade6771f3887846ce43ca4b
bluexuchun <345065695@qq.com>
2018-11-16 11:07:21 +0800

    colorControl

--- app/src/main/java/widiazine/bluexuchun/workapp/widget/TextGradient.kt
+++ app/src/main/java/widiazine/bluexuchun/workapp/widget/TextGradient.kt
@@ -5,14 +5,29 @@ import android.graphics.Color
 import android.graphics.LinearGradient
 import android.graphics.Shader
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.TextView
+import widiazine.bluexuchun.workapp.R
 
-open class TextGradient(context: Context, attrs: AttributeSet? = null) : TextView(context, attrs) {
+class TextGradient(context: Context) : TextView(context){
 
+    var color1 = 0
+    var color2 = 0
+
+    constructor(context: Context,attributeSet: AttributeSet):this(context){
+        val arrayType = context.obtainStyledAttributes(attributeSet, R.styleable.TextGradient)
+
+        color1 = arrayType.getColor(R.styleable.TextGradient_color1,0)
+        color2 = arrayType.getColor(R.styleable.TextGradient_color2,0)
+        var test = Color.parseColor("#FDCC30")
+        Log.d("color1","${color1}")
+        Log.d("color","${test}")
+    }
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         if(changed){
-            paint.setShader(LinearGradient(0.toFloat(),0.toFloat(),measuredWidth.toFloat(),0.toFloat(), Color.parseColor("#FDCC30"),Color.parseColor("#FD9407"),Shader.TileMode.CLAMP))
+            Log.d("color1","${color1}")
+            paint.setShader(LinearGradient(0.toFloat(),0.toFloat(),measuredWidth.toFloat(),0.toFloat(), -144336,-144336,Shader.TileMode.CLAMP))
         }
     }
 }
